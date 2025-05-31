eb50e08d3c56e730aba01bfaa53f6b771d78394d
uydev <lequocuyit@gmail.com>
2025-05-31 15:59:54 +0700

    Update CI/CD workflow and enhance Explore Page layout
    - Modified the Docker CI/CD workflow to change the directory path for deployment from `/devops/skillswap` to `/devops-vps/skillswap`, ensuring correct project structure.
    - Refactored the Explore Page component to include a Header and improved layout, enhancing user navigation and overall presentation.
    - Updated the RetroGrid component's background gradient for better visual consistency across themes.

--- apps/web/src/app/explore/page.tsx
+++ apps/web/src/app/explore/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
-export const Page = () => {
+import { Header } from '@/components/header'
+export default function ExplorePage() {
   return (
-    <div>Explorer Page</div>
+    <div>
+      <Header />
+      <main>
+        <div className='container mx-auto'>
+            <h1 className='text-4xl font-bold'>Explore</h1>
+        </div>
+      </main>
+    </div>
   )
 }
