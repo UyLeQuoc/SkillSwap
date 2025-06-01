0d7737b8ca1357aa40297a1c19ef55dba302ffdf
uydev <lequocuyit@gmail.com>
2025-06-01 17:48:07 +0700

    Refactor CORS Configuration in API for Simplified Setup
    - Updated the CORS settings in the API to enable all origins, simplifying the configuration process.
    - This change enhances flexibility for cross-origin requests while maintaining the ability to handle credentials.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,23 +5,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
 
     // Configure CORS with specific options
-    app.enableCors({
-        origin: ["https://skillswap.uydev.id.vn", "http://localhost:3000"],
-        credentials: true,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-        allowedHeaders: [
-            "Origin",
-            "X-Requested-With",
-            "Content-Type",
-            "Accept",
-            "Authorization",
-            "Access-Control-Allow-Origin",
-            "Access-Control-Allow-Credentials"
-        ],
-        exposedHeaders: ["Authorization"],
-        preflightContinue: false,
-        optionsSuccessStatus: 204
-    })
+    app.enableCors(true)
 
     await app.listen(process.env.PORT ?? 4000)
 }
